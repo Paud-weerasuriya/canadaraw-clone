353ea1a2799af55e0502534f50d0ab9510df6414
Paud-weerasuriya <udaraweerasuriya2@gmail.com>
2023-05-16 11:30:29 +0530

    updated

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="overflow-hidden pb-5">
+<div class="overflow-hidden pb-3 bg-black">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7926.245129068641!2d79.85336762775762!3d6.921295062432384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259e91b0f9869%3A0x49af8f3e7790836c!2sColombo%2C%20Sri%20Lanka!5e0!3m2!1sen!2sus!4v1620638268565!5m2!1sen!2sus"  class="w-full h-96"></iframe>
 </div>
     <div class="relative bg-cover bg-center h-screen bg-fixed" style="background-image: url('https://cdn.sanity.io/images/0vv8moc6/dvm360/0d51a75379a3525815b0517d4f7472c2917a791f-1920x1080.png')">
@@ -20,7 +20,7 @@
         </div>
       </div>
       
-<footer class="py-20 bg-black pt-20 mt-10">
+<footer class="py-20 bg-black">
   <div class="max-w-screen-xl mx-auto flex justify-between space-x-14">
       <div class="flex flex-col space-y-6 w-1/4">
         <a href="/"> <img src="/images/logo.png" alt="screen logo" width={180} height={13}/></a>
